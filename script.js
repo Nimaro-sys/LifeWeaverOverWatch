b3d84bf3923269f8755b006008868f719e41882e
Nimaro-sys <romain.hermenegilde.dwwm48@gmail.com>
2024-12-16 10:38:39 +0100

    Ajout du bouton Secret

---
+++ script.js
@@ -0,0 +1,11 @@
+/// Bouton Secret
+
+function secret_button() {
+    const secretButton = document.querySelector('.secret__button');
+
+    const secretText = "Est le personnage masculin d'overwatch le plus joué par des femmes.";
+
+    secretButton.textContent = secretText;
+}
+
+///
