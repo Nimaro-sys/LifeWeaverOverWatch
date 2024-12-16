638cb43dd3eb382e737760fb74a6284576e62093
Nimaro-sys <romain.hermenegilde.dwwm48@gmail.com>
2024-12-16 11:50:40 +0100

    Ajout breakpoint

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 function secret_button() {
     const secretButton = document.querySelector('.secret__button');
-
     const secretText = "Est le personnage masculin d'overwatch le plus joué par des femmes.";
 
     secretButton.textContent = secretText;
