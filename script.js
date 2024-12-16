9d8c775e118317135c21b446100162a6fb9450cb
Nimaro-sys <romain.hermenegilde.dwwm48@gmail.com>
2024-12-16 15:12:21 +0100

    Update

--- script.js
+++ script.js
@@ -7,4 +7,32 @@ function secret_button() {
     secretButton.textContent = secretText;
 }
 
-/// A suivre...
+/// Alertes quand le bouton les champs ne sont correctements remplies
+
+function VerifInput() {
+    var nameField = document.getElementById("name");
+    var emailField = document.getElementById("email");
+
+    const textarea = document.getElementById("message");
+    const message = textarea.value.trim();
+    const minLength = 50;
+
+    if (nameField.value.trim().length < 3) {
+        alert("Votre prénom et nom doivent contenir au moins 3 lettres.");
+        return;
+    }
+
+    if (!nameField.value || !emailField.value) {
+        alert("Tous les champs doivent être remplis.");
+        return;
+    }
+
+    if (message.length < minLength) {
+        alert("Vous devez écrire au moins 50 caractères dans votre message.");
+        return;
+    }
+        alert("Message envoyé avec succès !");
+    };
+    
+/// A suivre..
+
